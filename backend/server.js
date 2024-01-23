13705b5d51a5df226bd4766f2785dfc7bc16cf82
rdsub <subhashana77@gmail.com>
2024-01-23 23:59:49 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+//connect to express app
+const app = express();
+
+//connect to MongoDB
+
+
+//middleware
+
+
+//SCHEMA
+
+
+//Routs
+
+app.listen(3001);
